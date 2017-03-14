a4fb98b2fff13af17635fd3e3481601132c8af93
Denis Avdeev <davdeev@productengine.com>
2017-03-14 22:14:45 +0200

    https with auto certificates generation (initial version)

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sleep 8
 cd /home/pi/Watchdog/watchdog/web
-sudo nohup node ./bin/www > ./output.log 2> error.log &
+sudo nohup node ./bin/www isProduction=true > ./output.log 2> error.log &
 sleep 10
 sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -N -R 3333:localhost:8080 -p 2244 rssh@j1.qr-code.ws &
 sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -N -R 3334:localhost:8081 -p 2244 rssh@j1.qr-code.ws &
