6d27c994c834c7ef277a0d768a8932de42aad12e
ADenisUA <adenisua@gmail.com>
2017-03-14 21:27:15 +0200

    auto ssh for port 22

--- startup.sh
+++ startup.sh
@@ -3,6 +3,6 @@ sleep 8
 cd /home/pi/Watchdog/watchdog/web
 sudo nohup node ./bin/www > ./output.log 2> error.log &
 sleep 10
-#sudo autossh -M 0 -f -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -R 3333:localhost:80 -p 2244 rssh@j1.qr-code.ws &
-#ssh -nNT -R 3333:localhost:80 -p 2244 rssh@j1.qr-code.ws &
-sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -N -R 3333:localhost:80 -p 2244 rssh@j1.qr-code.ws &
+#sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -N -R 80:localhost:8080 -p 2244 rssh@j1.qr-code.ws &
+sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -N -R 3333:localhost:8080 -p 2244 rssh@j1.qr-code.ws &
+sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -N -R 3334:localhost:22 -p 2244 rssh@j1.qr-code.ws &
