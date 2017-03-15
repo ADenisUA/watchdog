5a3753dc092dc93caaf583bd750cf8e8a5e8f5c4
Denis Avdeev <davdeev@productengine.com>
2017-03-15 23:33:37 +0200

    ui minor improvements

--- web/static/js/Api.js
+++ web/static/js/Api.js
@@ -107,7 +107,7 @@ class Api {
 
     stopStreaming() {
         $("#streamingView").remove();
-        $("#streamingContainer").append('<img id="streamingView" src="/snapshot/snapshot.jpeg" width="100%" height="100%" />');
+        $("#streamingContainer").append('<img id="streamingView" src="/stream/snapshot.jpeg" width="100%" height="100%" />');
     }
 
     startStreaming() {
