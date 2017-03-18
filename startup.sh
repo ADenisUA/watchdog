8215fd77079d997aa1d4d7da87abb95faeb9bed7
Denis Avdeev <davdeev@productengine.com>
2017-03-18 20:23:05 +0200

    new external ip/host

--- startup.sh
+++ startup.sh
@@ -4,8 +4,8 @@ sleep 8
 cd /home/pi/Watchdog/watchdog/web
 sudo nohup node ./bin/www isProduction=true > ./output.log 2> error.log &
 sleep 10
-sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -N -R 3333:localhost:80 -p 2244 rssh@j1.qr-code.ws &
-sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -N -R 3334:localhost:443 -p 2244 rssh@j1.qr-code.ws &
+sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -N -R 80:localhost:80 root@107.181.154.239 &
+sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -N -R 443:localhost:443 root@107.181.154.239 &
 
 #sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-port 3333
 #sudo iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
