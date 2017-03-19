6093aa84c385543a7eb3f0674ca98a72a9dd83b7
Denis Avdeev <davdeev@productengine.com>
2017-03-19 09:57:57 +0200

    ssh tunneling

--- startup.sh
+++ startup.sh
@@ -8,6 +8,7 @@ sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3
 sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -N -R 443:localhost:443 root@107.181.154.239 &
 sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -N -R 3001:localhost:3001 root@107.181.154.239 &
 sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -N -R 3478:localhost:3478 root@107.181.154.239 &
+sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -N -R 2222:localhost:22 root@107.181.154.239 &
 
 #sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-port 3333
 #sudo iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
