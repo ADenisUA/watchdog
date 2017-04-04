535ca307b0d379372417d6d1aaa7a67516ff7ac9
Denis Avdeev <davdeev@productengine.com>
2017-04-04 14:13:24 +0300

    startup script - forward ports to 0.0.0.0

--- startup.sh
+++ startup.sh
@@ -4,11 +4,11 @@ sleep 8
 cd /home/pi/Watchdog/watchdog/web
 sudo nohup node ./bin/www isProduction=true > ./output.log 2> error.log &
 sleep 10
-sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R 155.94.169.47:80:localhost:80 -o ServerAliveInterval=30 root@findreview.info &
-sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R 155.94.169.47:443:localhost:443 -o ServerAliveInterval=30 root@findreview.info &
-sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R 155.94.169.47:3001:localhost:3001 -o ServerAliveInterval=30 root@findreview.info &
-sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R 155.94.169.47:3478:localhost:3478 -o ServerAliveInterval=30 root@findreview.info &
-sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R \*:2222:localhost:22 -o ServerAliveInterval=30 root@findreview.info &
+sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R 0.0.0.0:80:localhost:80 -o ServerAliveInterval=30 root@findreview.info &
+sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R 0.0.0.0:443:localhost:443 -o ServerAliveInterval=30 root@findreview.info &
+sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R 0.0.0.0:3001:localhost:3001 -o ServerAliveInterval=30 root@findreview.info &
+sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R 0.0.0.0:3478:localhost:3478 -o ServerAliveInterval=30 root@findreview.info &
+sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R 0.0.0.0:2222:localhost:22 -o ServerAliveInterval=30 root@findreview.info &
 
 #sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-port 3333
 #sudo iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
