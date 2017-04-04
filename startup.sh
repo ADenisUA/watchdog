d5fd50071cfc3872339fdb18e60fc888bc490551
Denis Avdeev <davdeev@productengine.com>
2017-04-04 22:52:07 +0300

    removed port 3428

--- startup.sh
+++ startup.sh
@@ -7,5 +7,5 @@ sleep 10
 sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R 0.0.0.0:80:localhost:80 -o ServerAliveInterval=30 root@findreview.info &
 sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R 0.0.0.0:443:localhost:443 -o ServerAliveInterval=30 root@findreview.info &
 sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R 0.0.0.0:3001:localhost:3001 -o ServerAliveInterval=30 root@findreview.info &
-sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R 0.0.0.0:3478:localhost:3478 -o ServerAliveInterval=30 root@findreview.info &
+#sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R 0.0.0.0:3478:localhost:3478 -o ServerAliveInterval=30 root@findreview.info &
 sudo -u pi autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "TCPKeepAlive yes" -N -R 0.0.0.0:2222:localhost:22 -o ServerAliveInterval=30 root@findreview.info &
